923732fab7b4c5c87c2586b6851baa506f48f060
cassiano-dio <cassiano@dio.me>
2022-10-20 12:03:25 -0300

    Update 2

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://ec2-54-166-139-226.compute-1.amazonaws.com:8080/api";
+const API_URL = "http://ec2-54-166-139-226.compute-1.amazonaws.com:8080/api/";
 
 const register = (username, email, password) => {
     return axios.post(API_URL + "auth/signup", {
